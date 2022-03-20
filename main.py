7c0d1f2643f98f908c4a6d88f61ee8b2f5bea6f1
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-03-20 21:58:16 +0300

    Added counter to main();

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 
 def get_vacancies(url, vacancy, area, period, page):
     url = url
-    params = {'text': vacancy, 'area': area, 'period': period, 'per_page': 100, 'page': page}
+    params = {'text': vacancy,
+              'area': area,
+              'period': period,
+              'per_page': 100,
+              'page': page,
+              'only_with_salary': 'true'}
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
@@ -22,19 +27,18 @@ def main():
     load_dotenv()
     area_Moscow = get_areas('https://api.hh.ru/suggests/areas', 'Москва')['items'][0]['id']
     page = 0
+    count = 0
     while True:
-        try:
-            vacancies = get_vacancies('https://api.hh.ru/vacancies', 'Python', area_Moscow, 1, page)
-            count = 0
-            for vacancy in vacancies['items']:
-                print(vacancy)
-                count += 1
-            page += 1
-            print(f'Page: {page}, elements: {count}')
-            if count < 100:
-                break
-        except requests.exceptions.HTTPError:
+        vacancies = get_vacancies('https://api.hh.ru/vacancies', 'Python', area_Moscow, 30, page)
+        for vacancy in vacancies['items']:
+            print(vacancy.get('name'), vacancy.get('salary'))
+            count += 1
+        page += 1
+        print(f'Page: {page}, elements: {count}')
+        if page == 20:
             break
+    #have to check parameter 'found', 'pages', 'per_page', 'page' for counter control
+    print(vacancies)
 
 
 if __name__ == '__main__':
