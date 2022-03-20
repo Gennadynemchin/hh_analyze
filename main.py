711d7e3b720212aaf36d1bd590e433fbf65d57b1
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-03-20 21:21:31 +0300

    Added get_areas(), get_vacancies() funcs;

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_positions(url, position):
+def get_vacancies(url, vacancy, area, period, page):
     url = url
-    params = {'text': position}
+    params = {'text': vacancy, 'area': area, 'period': period, 'per_page': 100, 'page': page}
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
@@ -20,9 +20,19 @@ def get_areas(url, area):
 
 def main():
     load_dotenv()
-    print(get_positions('https://api.hh.ru/suggests/positions', 'Программист'))
-    print(get_areas('https://api.hh.ru/suggests/areas', 'Москва'))
-    return
+    area_Moscow = get_areas('https://api.hh.ru/suggests/areas', 'Москва')['items'][0]['id']
+    page = 0
+    while True:
+        try:
+            vacancies = get_vacancies('https://api.hh.ru/vacancies', 'Python', area_Moscow, 1, page)
+            count = 0
+            for vacancy in vacancies['items']:
+                print(vacancy['name'], vacancy['salary'])
+                count += 1
+            page += 1
+            print(f'Page: {page}, elements: {count}')
+        except requests.exceptions.HTTPError:
+            break
 
 
 if __name__ == '__main__':
