8c63e698d4c4851acbf0d824127d3a3eeb01cc29
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-03-21 21:49:22 +0300

    Added counter to main();

--- main.py
+++ main.py
@@ -15,30 +15,20 @@ def get_vacancies(url, vacancy, area, period, page):
     return response.json()
 
 
-def get_areas(url, area):
-    url = url
-    params = {'text': area}
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
-
-
 def main():
     load_dotenv()
-    area_Moscow = get_areas('https://api.hh.ru/suggests/areas', 'Москва')['items'][0]['id']
+    languages = ['Go', 'C', 'C#', 'C++', 'PHP', 'Ruby', 'Python', 'Java', 'JavaScript']
     page = 0
     count = 0
     while True:
-        vacancies = get_vacancies('https://api.hh.ru/vacancies', 'Python', area_Moscow, 30, page)
+        vacancies = get_vacancies('https://api.hh.ru/vacancies', 'Программист Python', 1, 30, page)
         for vacancy in vacancies['items']:
             print(vacancy.get('name'), vacancy.get('salary'))
             count += 1
         page += 1
         print(f'Page: {page}, elements: {count}')
-        if page == 20:
+        if page == vacancies['pages']:
             break
-    #have to check parameter 'found', 'pages', 'per_page', 'page' for counter control
-    print(vacancies)
 
 
 if __name__ == '__main__':
